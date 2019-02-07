28f76a2000fd0969d425cde719e0bf5fc85e7852
agoncalvesnerd <agoncalves@nerd.com>
2019-02-07 16:46:15 +0000

    Moved contract files.

---
+++ contracts/Blackjack.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.25;
+
+import "./helpers/safemath.sol";
+
+contract Blackjack {
+
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+
+    struct User {
+        uint32 winningHands;
+        uint32 losingHands;
+        uint256 registerDate;
+    }
+
+    // simple mapping of user data
+    mapping (address => User) users;
+
+    
+
+}
